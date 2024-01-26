00e027828d5c0276607037fb23a073381229af06
0xripleys <0xripleys@solend.fi>
2024-01-26 15:20:47 -0500

    fix

--- suilend-cli/src/sdk/suilend/init.ts
+++ suilend-cli/src/sdk/suilend/init.ts
@@ -2,23 +2,23 @@ import * as decimal from "./decimal/structs";
 import * as lendingMarket from "./lending-market/structs";
 import * as obligation from "./obligation/structs";
 import * as reserve from "./reserve/structs";
-import { StructClassLoader } from "../_framework/loader";
+// import { StructClassLoader } from "../_framework/loader";
 
-export function registerClasses(loader: StructClassLoader) {
-  loader.register(decimal.Decimal);
-  loader.register(reserve.Price);
-  loader.register(reserve.CToken);
-  loader.register(reserve.InterestRateModel);
-  loader.register(reserve.Reserve);
-  loader.register(reserve.ReserveConfig);
-  loader.register(reserve.ReserveTreasury);
-  loader.register(obligation.Borrow);
-  loader.register(obligation.Deposit);
-  loader.register(obligation.Key);
-  loader.register(obligation.Obligation);
-  loader.register(obligation.RefreshedTicket);
-  loader.register(lendingMarket.Name);
-  loader.register(lendingMarket.LendingMarket);
-  loader.register(lendingMarket.LendingMarketOwnerCap);
-  loader.register(lendingMarket.ObligationOwnerCap);
-}
+// export function registerClasses(loader: StructClassLoader) {
+//   loader.register(decimal.Decimal);
+//   loader.register(reserve.Price);
+//   loader.register(reserve.CToken);
+//   loader.register(reserve.InterestRateModel);
+//   loader.register(reserve.Reserve);
+//   loader.register(reserve.ReserveConfig);
+//   loader.register(reserve.ReserveTreasury);
+//   loader.register(obligation.Borrow);
+//   loader.register(obligation.Deposit);
+//   loader.register(obligation.Key);
+//   loader.register(obligation.Obligation);
+//   loader.register(obligation.RefreshedTicket);
+//   loader.register(lendingMarket.Name);
+//   loader.register(lendingMarket.LendingMarket);
+//   loader.register(lendingMarket.LendingMarketOwnerCap);
+//   loader.register(lendingMarket.ObligationOwnerCap);
+// }
